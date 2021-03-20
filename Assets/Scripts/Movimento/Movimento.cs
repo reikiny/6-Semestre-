13e96c42602d9feaf795789689f537b138693f9e
reikiny <50430645+reikiny@users.noreply.github.com>
2021-03-20 16:25:39 -0300

    arrumar

--- Assets/Scripts/Movimento/Movimento.cs
+++ Assets/Scripts/Movimento/Movimento.cs
@@ -9,56 +9,50 @@ public class Movimento : MonoBehaviour
     public bool baixo;
     public bool esquerda;
     public bool direita;
-    Vector3 target;
-    public float step;
+    //Vector3 target;
+    //public float step;
+    public float distace;
 
    
     private void Update()
     {
         Rayteste();
-        if (Input.GetMouseButtonDown(0))
-        {
-            Mover();
+        //if (Input.GetMouseButtonDown(0))
+        //{
+            //Mover();
             
-        }      
+        //}      
     }
     void Rayteste()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.forward, out hit, 1f) 
-            || Physics.Raycast(transform.position, Vector3.back, out hit, 1f )
-            || Physics.Raycast(transform.position, Vector3.left, out hit, 1f ) 
-            || Physics.Raycast(transform.position, Vector3.right, out hit, 1f))
+        if (cima && Physics.Raycast(transform.position, Vector3.forward, out hit, distace))
         {
-            if (hit.collider.tag == "Andar")
-            {
-                hit.collider.tag = "Click";
-            }
-            else
-            {
-               
-            }
-           Debug.DrawRay(transform.position, Vector3.forward * hit.distance, Color.yellow);
-           Debug.DrawRay(transform.position, Vector3.back * hit.distance, Color.yellow);
-           Debug.DrawRay(transform.position, Vector3.left * hit.distance, Color.yellow);
-           Debug.DrawRay(transform.position, Vector3.right * hit.distance, Color.yellow);
-           
+            Debug.DrawRay(transform.position, Vector3.forward * hit.distance, Color.yellow);
         }
-        else
+        if (baixo && Physics.Raycast(transform.position, Vector3.back, out hit, distace))
         {
-            
+            Debug.DrawRay(transform.position, Vector3.back * hit.distance, Color.yellow);
         }
+        if (esquerda && Physics.Raycast(transform.position, Vector3.left, out hit, distace))
+        {
+            Debug.DrawRay(transform.position, Vector3.left * hit.distance, Color.yellow);
+        }
+        if (direita && Physics.Raycast(transform.position, Vector3.right, out hit, distace))
+        {
+            Debug.DrawRay(transform.position, Vector3.right * hit.distance, Color.yellow);
+        }  
     }
 
-    void Mover()
-    {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Click")
-        {
-            
-            target = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
-            transform.position = Vector3.MoveTowards(transform.position, target, step);
-        }
-    }
+    //void Mover()
+    //{
+    //    RaycastHit hit;
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Click")
+    //    {
+
+    //        target = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
+    //        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    //    }
+    //}
 }
