69cff6bd41370f39de4bc358381e0fc90f019fe3
FariaMASTER <luk.0011@hotmail.com>
2021-03-21 23:08:22 -0300

    Escudeiro Para melhorar

--- Assets/Scripts/Escudeiro.cs
+++ Assets/Scripts/Escudeiro.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Escudeiro : MonoBehaviour
 {
     Vector3 target;
@@ -9,40 +9,60 @@ public class Escudeiro : MonoBehaviour
     GameObject antigo;
 
     bool ativo;
+    bool posicionado;
 
     void Update()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //poder usar o botao se for o turno do player e se nao tiver posicionado
+        
+        if (Turns.playerTurn && !posicionado)
+            GetComponent<Button>().interactable = true;
+
+        else
+            GetComponent<Button>().interactable = false;
+
         if (ativo)
         {
-            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Tile"))
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Tile") && hit.collider.gameObject.GetComponent<Tile>().agentes == Agentes.Vazio)
             {
-                if (hit.collider.gameObject.GetComponent<Tile>().agentes == Agentes.Vazio && Turns.playerTurn)
+                //feedback
+                if (antigo != hit.collider.gameObject)
+                {
+                    if (antigo != null)
+                    {
+                        antigo.GetComponent<Renderer>().material.color = Color.white;
+                    }
+                    antigo = hit.collider.gameObject;
+                    hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                }
+                //posicionar
+                if (Input.GetMouseButtonDown(0))
                 {
                     Turns.playerTurn = false;
 
                     target = new Vector3(hit.transform.position.x, hit.transform.position.y + .5f, hit.transform.position.z);
                     Instantiate(jons, target, Quaternion.identity);
+                    //terminar feedback
+                    antigo.GetComponent<Renderer>().material.color = Color.white;
+                    antigo = null;
+                    //pos
+
+                    posicionado = true;
                     ativo = !ativo;
                 }
             }
-            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Tile"))
+            //resetar feedback
+            else 
             {
-                if (antigo != hit.collider.gameObject)
+                if (antigo != null)
                 {
-                    if (antigo != null)
-                    {
-                        hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.white;
-                    }
-                    antigo = hit.collider.gameObject;
-                    hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    antigo.GetComponent<Renderer>().material.color = Color.white;
+                    antigo = null;
                 }
-            }
-            else if (hit.collider != null)
-            {
-                hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.white;
-                antigo = null;
             }
         }
     }
