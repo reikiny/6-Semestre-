ff0867c7a3bc3bedc62f5d118645ed0f2094554f
FariaMASTER <luk.0011@hotmail.com>
2021-03-22 06:54:19 -0300

    Level Creator

---
+++ Assets/Scripts/ColorToPrefab.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[System.Serializable]
+public class ColorToPrefab
+{
+   
+    public Color color;
+    public GameObject prefab;
+
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+
+    public float multiplicador;
+
+    public bool instantiated;
+
+    void Start()
+    {
+        if (!instantiated)
+        {
+            GenerateLevel();
+            instantiated = true;
+        }
+
+    }
+
+    public void GenerateLevel()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+                
+            }
+        }
+    }
+
+
+    public void GenerateTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        if (pixelColor.a == 0)
+        {
+            //pixel é transparente. FAZ NADA
+
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x , 0, y );
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
